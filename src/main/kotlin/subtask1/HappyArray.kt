85064ab8556d05b183e397bc216a745a14259045
cris1972 <cris72@mail.ru>
2021-05-23 19:16:49 +0300

    work onli array without 2test

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,58 @@
 package subtask1
 
+import javax.print.attribute.IntegerSyntax
+
 class HappyArray {
 
-    // TODO: Complete the following function
+
+   lateinit var  happi1: IntArray
+
+    var index = 0
+    lateinit var myArray: IntArray
+
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var happi:IntArray
+        var badArray1 = intArrayOf()
+
+
+
+            happi1 = myHappy(sadArray)
+            index = 0
+
+            happi = myHappy(happi1)
+            while (index != 0) {
+                happi = myHappy(happi)
+                happi = myHappy(happi)
+            }
+
+
+            print("index     =  $index")
+
+
+
+            return happi
+
+
+    }
+
+    private fun myHappy(sadArray: IntArray): IntArray {
+
+
+        myArray = intArrayOf(sadArray[0])
+        for (i in 1..sadArray.size - 2) {
+            index=0
+            if (sadArray[i] < (sadArray[i - 1] + sadArray[i + 1])) {
+               myArray += sadArray[i]
+
+            }
+            else{index++}
+        }
+
+        myArray  += sadArray[sadArray.size - 1]
+       print(myArray.joinToString())
+        println()
+
+        return myArray
     }
 }
+
