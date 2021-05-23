a3bfbe51c0041fb08ac5b8afd4e720694c0418e7
cris1972 <cris72@mail.ru>
2021-05-23 20:55:47 +0300

    work BillCounter and HappyArray

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -1,10 +1,31 @@
 package subtask2
 
 class BillCounter {
-
+    var res= 0
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+
+       countRright(bill,k)
+        var resaultBill: String
+
+        if(res==b)
+        {
+
+       return  "bon appetit"}
+        else{
+            res=b-res
+            return  res.toString()
+        }
+    }
+
+    private fun countRright(bill: IntArray, k:Int) {
+        bill[k] = 0
+
+        for(i in 0..bill.size - 1){
+            res=res+bill[i]
+        }
+        res /= 2
+
     }
 }
