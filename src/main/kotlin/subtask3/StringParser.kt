c1d2271a5797af6c407f442cdd64e1bf4e0c0b9a
cris1972 <cris72@mail.ru>
2021-05-24 21:01:12 +0300

    work StringParser onli one test

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,9 +1,36 @@
 package subtask3
 
 class StringParser {
-
+    var result = arrayOf("simple", "input", "string")
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
+        var result1 = myparseString(inputString)
+        result1.forEach {
+            println(it)
+        }
+       // print(result1.joinToString())
+        return result1
+    }
+
+    private fun myparseString(inputString: String): Array<String> {
+        var splitString: Array<String>
+       var splitString2 = arrayOf<String>()
+        splitString = inputString.split("{","(","[","<").toTypedArray()
+        for (i in splitString.indices)
+            if (splitString[i].indexOfAny(charArrayOf('}', ')', '>',']')) >= 0) {
+
+                splitString[i] = splitString[i].replace("}", "")
+                splitString[i] = splitString[i].replace(")", "")
+                splitString[i] = splitString[i].replace(">", "")
+                splitString[i] = splitString[i].replace("]", "")
+
+                splitString2 += splitString[i].trim()
+                   // println(splitString[i])
+
+
+            }
+
+        return splitString2
     }
 }
